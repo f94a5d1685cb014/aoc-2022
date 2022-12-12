f962f2fb65077de85bbefb9e2d0caa28dfca5fe8
Mohamed A. Benhamida <mohamed.a.benhamida@gmail.com>
2022-12-12 23:46:51 +0100

    day 02.

--- day02.go
+++ day02.go
@@ -1,5 +1,51 @@
 package main
 
+var oponentTrick = map[byte]int{'A': 1, 'B': 2, 'C': 3}
+var myTrick = map[byte]int{'X': 1, 'Y': 2, 'Z': 3}
+var losingOrder = map[int]int{1: 3, 2: 1, 3: 2}
+var winingOrder = map[int]int{3: 1, 1: 2, 2: 3}
+
 func Day02() {
-	print("hello world from day2 file.")
+	data := ReadInput("input/day02.txt")
+
+	println("Day 02 - Part 01: ", part01(data))
+	println("Day 02 - Part 01: ", part02(data))
+}
+
+func part01(data []string) int {
+	var score int
+
+	for _, round := range data {
+		if oponentTrick[round[0]] == myTrick[round[2]] { // Draw
+			score += 3
+		} else {
+			if losingOrder[oponentTrick[round[0]]] == myTrick[round[2]] { // Loss
+				score += 0
+			} else { // Win
+				score += 6
+			}
+		}
+		score += myTrick[round[2]]
+	}
+
+	return score
+}
+
+func part02(data []string) int {
+	var score int
+
+	for _, round := range data {
+		if round[2] == 'X' { // Loss
+			score += 0
+			score += losingOrder[oponentTrick[round[0]]]
+		} else if round[2] == 'Y' { // Draw
+			score += 3
+			score += oponentTrick[round[0]]
+		} else if round[2] == 'Z' { // Win
+			score += 6
+			score += winingOrder[oponentTrick[round[0]]]
+		}
+	}
+
+	return score
 }
--- main.go
+++ main.go
@@ -1,6 +1,35 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
 	Day01()
 	Day02()
+}
+
+func ReadInput(filename string) []string {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "opening file:", err)
+	}
+
+	defer file.Close()
+
+	var data []string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		data = append(data, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+
+	return data
 }
